0004cae01b0454f16b80387f6f3fec12d54f08f7
royweb <xytank@gmail.com>
2019-11-29 15:51:19 -0500

    Set up socket and connect to the server

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+let socket;
+
 const Chat = ({ location }) => {
     // [state, method]
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+
+    const ENDPOINT = 'localhost:8000';
 
     // same as componentDidMount
     useEffect(() => {
@@ -13,9 +17,16 @@ const Chat = ({ location }) => {
         // location is from react-router-dom
         const { name, room } = queryString.parse(location.search);
 
+        // connect to the server
+        socket = io(ENDPOINT);
+
         setName(name);
         setRoom(room);
-    });
+
+        console.log(socket);
+
+        // excute this function only if these values change
+    }, [ENDPOINT, location.search]);
 
     return(
         <h1>Chat</h1>
