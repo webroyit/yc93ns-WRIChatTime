355376e0d2709a930cb59315d8df0d34765d7f75
royweb <xytank@gmail.com>
2019-11-29 15:42:05 -0500

    Use useEffect() to get the values from the query params

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+    // [state, method]
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    // same as componentDidMount
+    useEffect(() => {
+        // queryString convert the query params into JS object
+        // location is from react-router-dom
+        const { name, room } = queryString.parse(location.search);
+
+        setName(name);
+        setRoom(room);
+    });
+
     return(
         <h1>Chat</h1>
     );
