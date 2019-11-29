bcca5735e58f65f81eef361c297dd487bf7cd750
royweb <xytank@gmail.com>
2019-11-29 14:27:34 -0500

    Add content for join page

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+    // state
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return(
-        <h1>Join</h1>
+        <div className="join">
+            <div className="join__container">
+                <h1 className="join__heading">Join Chat Time</h1>
+                <div>
+                    <input
+                        placeholder="Name"
+                        className="join__input"
+                        type="text" />
+                </div>
+                <div>
+                    <input
+                        placeholder="Room"
+                        className="join__input"
+                        type="text" />
+                </div>
+                <Link>
+                    <button className="join__button" type="submit">Join Chatroom</button>
+                </Link>
+            </div>
+        </div>
     );
 };
 
