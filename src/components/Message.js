4de03a576882baf75222d977f1df51c7c642893f
royweb <xytank@gmail.com>
2019-12-01 12:41:21 -0500

    Display the messages from the server

---
+++ src/components/Message.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// message that is being pass is an object
+const Message = ({ message: { user, text }, name }) => {
+    const trimName = name.trim().toLowerCase();
+    let isSetByCurrentUser = false;
+
+    // show the message on the left or right
+    if(user === trimName){
+        isSetByCurrentUser = true;
+    }
+
+    return(
+        isSetByCurrentUser ? (
+            <div className="message startRight">
+                <p className="message__name">{trimName}</p>
+                <div className="message__box">
+                    <p className="message__text">{text}</p>
+                </div>
+            </div>
+        ) : (
+            <div className="message startLeft">
+                <div className="message__box">
+                    <p className="message__text">{text}</p>
+                </div>
+                <p className="message__name">{user}</p>
+            </div>
+        )
+    );
+};
+
+export default Message;
